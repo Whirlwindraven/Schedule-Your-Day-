ecc566592dcee9b7622b9bdf1b4355efe82183a6
Whirlwindraven <fadysherif1998@gmail.com>
2023-02-26 21:09:05 -0500

    fixed display timeday, updated the time block colors based on past,present, and current, and addedan event listner to the save button

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,31 @@ const displayTime = document.getElementById('currentDay');
 const timeBlocks = document.querySelectorAll('.time-block');
 
 // Set the current day at the top of the calendar
-displayTime.textContent = dayjs().format('dddd, MMMM Do');
+displayTime.textContent = dayjs().format('dddd, MMMM DD');
 
+// Add event listeners to the time blocks
+timeBlocks.forEach((timeBlock) => {
+  const saveButton = timeBlock.querySelector('.saveBtn');
+  const textField = timeBlock.querySelector('.description');
+  const rowId = timeBlock.getAttribute('id');
+  const rowTime = parseInt(rowId.replace('hour-', ''), 10);
 
+  // Load any saved data from localStorage
+  textField.value = localStorage.getItem(rowId);
+
+  // Update the color of the time block based on the current time
+  const currentHour = dayjs().hour();
+
+  if (rowTime < currentHour) {
+    timeBlock.classList.add('past');
+  } else if (rowTime === currentHour) {
+    timeBlock.classList.add('present');
+  } else {
+    timeBlock.classList.add('future');
+  }
+
+  // Save the text for the event in local storage
+  saveButton.addEventListener('click', () => {
+    localStorage.setItem(rowId, textField.value);
+  });
+});
